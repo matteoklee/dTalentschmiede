be4d0841b0293b0876c71d33e35c70dcfc86a648
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-08-20 03:07:01 +0200

    first commit

--- src/main/java/de/dataport/dtalentschmiede/DTalentschmiedeApplication.java
+++ src/main/java/de/dataport/dtalentschmiede/DTalentschmiedeApplication.java
@@ -8,6 +8,12 @@ public class DTalentschmiedeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DTalentschmiedeApplication.class, args);
+        System.out.println("      _ _____     _            _            _               _          _      \n" +
+                "   __| |_   _|_ _| | ___ _ __ | |_ ___  ___| |__  _ __ ___ (_) ___  __| | ___ \n" +
+                "  / _` | | |/ _` | |/ _ \\ '_ \\| __/ __|/ __| '_ \\| '_ ` _ \\| |/ _ \\/ _` |/ _ \\\n" +
+                " | (_| | | | (_| | |  __/ | | | |_\\__ \\ (__| | | | | | | | | |  __/ (_| |  __/\n" +
+                "  \\__,_| |_|\\__,_|_|\\___|_| |_|\\__|___/\\___|_| |_|_| |_| |_|_|\\___|\\__,_|\\___|\n" +
+                "                                                                              ");
     }
 
 }
