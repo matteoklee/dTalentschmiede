4c28c5acf4414350940137f1fab87a3ce67a2214
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-08-20 03:03:21 +0200

    initial commit

---
+++ src/main/java/de/dataport/dtalentschmiede/DTalentschmiedeApplication.java
@@ -0,0 +1,13 @@
+package de.dataport.dtalentschmiede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DTalentschmiedeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DTalentschmiedeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/dataport/dtalentschmiede/DTalentschmiedeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dataport.dtalentschmiede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DTalentschmiedeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
